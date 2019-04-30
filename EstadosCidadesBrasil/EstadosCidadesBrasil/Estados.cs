7a00d66ce7965412b51500fae7f284e007802356
Michael Pedro - EMPRESA <mchdevsoftware@gmail.com>
2019-04-30 11:28:49 -0300

    *Foi adicionado uma classe [Estados] que armazenará os estados pertencentes ao país do Brasil.
    *Foi adicionado uma classe [Cidades] que armazenará as cidades pertencentes aos estados do Brasil.
    
    *Foi implementado a herança na classe principal da dll [EstadosCidadesBrasil].

---
+++ EstadosCidadesBrasil/EstadosCidadesBrasil/Estados.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EstadosCidadesBrasil
+{
+    public abstract class Estados
+    {
+        private string[] statesOfBrazil;
+
+        /// <summary>
+        /// Retorna todos os estados do Brasil em um componente ComboBox.
+        /// </summary>
+        /// <param name="cb"></param>
+        public virtual ComboBox EstadosBrasil(ComboBox cb)
+        {
+            this.statesOfBrazil = new string[]
+            {
+                "Acre",
+                "Alagoas",
+                "Amapá",
+                "Amazonas",
+                "Bahia",
+                "Ceará",
+                "Distrito Federal",
+                "Espírito Santo",
+                "Goiás",
+                "Maranhão",
+                "Mato Grosso",
+                "Mato Grosso do Sul",
+                "Minas Gerais",
+                "Pará",
+                "Paraíba",
+                "Paraná",
+                "Pernambuco",
+                "Piauí",
+                "Rio de Janeiro",
+                "Rio Grande do Norte",
+                "Rio Grande do Sul",
+                "Rondônia",
+                "Roraima",
+                "Santa Catarina",
+                "São Paulo",
+                "Sergipe",
+                "Tocantins"
+             };
+
+            cb.Items.Add(statesOfBrazil);
+
+            return cb;
+        }
+    }
+}
---
+++ EstadosCidadesBrasil/EstadosCidadesBrasil/EstadosCidadesBrasil.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EstadosCidadesBrasil
+{
+    public class EstadosCidadesBrasil
+    {
+        EstadosDoBrasil states;
+        CidadesDosEstadosDoBrasil cities;  
+        
+        public ComboBox EstadosDoBrasil (ComboBox estadosBrasil)
+        {
+            states = new EstadosDoBrasil();
+
+            states.EstadosBrasil(estadosBrasil);
+
+            return estadosBrasil;
+        }
+    }
+
+    public partial class CidadesDosEstadosDoBrasil : Cidades
+    {
+
+    }
+
+    public partial class EstadosDoBrasil : Estados
+    {
+        public override ComboBox EstadosBrasil(ComboBox cb)
+        {
+            return base.EstadosBrasil(cb);
+        }
+    }
+}
