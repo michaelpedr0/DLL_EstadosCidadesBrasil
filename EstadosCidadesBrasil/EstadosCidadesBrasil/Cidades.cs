9134775b16d3ea9b4612d185ce5db57acec882ea
Michael Pedro - EMPRESA <mchdevsoftware@gmail.com>
2019-04-30 11:42:32 -0300

    *Foi adicionado na classe [Cidades] as cidades pertencentes à Alagoas.

--- EstadosCidadesBrasil/EstadosCidadesBrasil/Cidades.cs
+++ EstadosCidadesBrasil/EstadosCidadesBrasil/Cidades.cs
@@ -48,6 +48,72 @@ namespace EstadosCidadesBrasil
             cb.Items.AddRange(citiesOfBrazil);
 
             return cb;
-        }        
+        }
+
+        /// <summary>
+        /// Retorna em um componente ComboBox a lista de cidades pertencentes ao estado de Alagoas.
+        /// </summary>
+        /// <param name="cb">Componente ComboBox que receberá o retorno das cidades.</param>
+        public virtual ComboBox Alagoas(ComboBox cb)
+        {
+            citiesOfBrazil = new string[]
+            {
+                "Água Branca", "Anadia", "Arapiraca", "Atalaia",
+
+                "Barra de Santo Antônio", "Barra de São Miguel", "Batalha", "Belém",
+                "Belo Monte", "Boca da Mata", "Branquinha",
+
+                "Cacimbinhas", "Cajueiro", "Campestre", "Campo Alegre", "Campo Grande",
+                "Canapi", "Capela", "Carneiros", "Chã Preta", "Coité do Nóia", "Colônia Leopoldina",
+                "Coqueiro Seco", "Coruripe", "Craíbas",
+
+                "Delmiro Gouveia", "Dois Riachos",
+
+                "Estrela de Alagoas",
+
+                "Feira Grande", "Feliz Deserto", "Flexeiras",
+
+                "Girau do Ponciano",
+
+                "Ibateguara", "Igaci", "Igreja Nova", "Inhapi",
+
+                "Jacaré dos Homens", "Jacuípe", "Japaratinga", "Jaramataia",
+                "Jequiá da Praia", "Joaquim Gomes", "Jundiá", "Junqueiro",
+
+                "Lagoa da Canoa", "Limoeiro de Anadia",
+
+                "Maceió", "Major Isidoro", "Mar Vermelho", "Maragogi", "Maravilha", "Marechal Deodoro",
+                "Maribondo", "Mata Grande", "Matriz de Camaragibe", "Messias", "Minador do Negrão",
+                "Monteirópolis", "Murici",
+
+                "Novo Lino",
+
+                "Olho dÁgua das Flores", "Olho dÁgua do Casado",
+                "Olho dÁgua Grande", "Olivença", "Ouro Branco",
+
+                "Palestina", "Palmeira dos Índios", "Pão de Açúcar", "Pariconha", "Paripueira",
+                "Passo de Camaragibe", "Paulo Jacinto", "Penedo", "Piaçabuçu", "Pilar",
+                "Pindoba", "Piranhas", "Poço das Trincheiras", "Porto Calvo", "Porto de Pedras",
+                "Porto Real do Colégio",
+
+                "Quebrangulo",
+
+                "Rio Largo", "Roteiro",
+
+                "Santa Luzia do Norte", "Santana do Ipanema", "Santana do Mundaú", "São Brás",
+                "São José da Laje", "São José da Tapera", "São Luís do Quitunde", "São Miguel dos Campos",
+                "São Miguel dos Milagres", "São Sebastião", "Satuba", "Senador Rui Palmeira",
+
+                "Tanque dArca", "Taquarana", "Teotônio Vilela", "Traipu",
+
+                "União dos Palmares",
+
+                "Viçosa"
+            };
+
+            cb.Items.AddRange(citiesOfBrazil);
+
+            return cb;
+        }
     }
 }
